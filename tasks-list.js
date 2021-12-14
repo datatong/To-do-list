4291b5ec39c0e726390f0d912903cfb2080e9da2
datatong <59157188+datatong@users.noreply.github.com>
2021-12-13 19:41:32 -0800

    Add files via upload

--- tasks-list.js
+++ tasks-list.js
@@ -1,10 +1,19 @@
 // ----- SELECT ITEMS -----
-
-// edit option
+const toDoForm = document.querySelector('.to-do-form');
+const toDo = document.getElementById('to-do');
+const submitBtn = document.getElementById('submit-btn');
+const container = document.querySelector('.task-container');
+const list = document.querySelector('.task-list');
+const clearAll = document.querySelector('.clear-all-btn');
 
 // ----- EVENT LISTENERS -----
+toDoForm.addEventListener('submit', addTask);
 
 // ----- FUNCTIONS -----
+function addTask(e) {
+    e.preventDefault();
+    console.log(toDo.value);
+}
 
 // ----- LOCAL STORAGE -----
 
