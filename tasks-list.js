920ae7c077bcee4683da36b68af1a5cc547a00e9
datatong <59157188+datatong@users.noreply.github.com>
2022-02-10 20:43:48 -0800

    Add files via upload

--- tasks-list.js
+++ tasks-list.js
@@ -1,4 +1,4 @@
-// ----- SELECT ITEMS -----
+// ***** SELECT ITEMS *****
 const toDoForm = document.querySelector('.to-do-form');
 const toDo = document.getElementById('to-do');
 const submitBtn = document.getElementById('submit-btn');
@@ -6,15 +6,72 @@ const container = document.querySelector('.task-container');
 const list = document.querySelector('.task-list');
 const clearAll = document.querySelector('.clear-all-btn');
 
-// ----- EVENT LISTENERS -----
+//edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ***** EVENT LISTENERS *****
+//submit form
 toDoForm.addEventListener('submit', addTask);
 
-// ----- FUNCTIONS -----
+// clear items
+
+
+// ***** FUNCTIONS *****
 function addTask(e) {
+    /*document.head.childNodes[1].href = "list-style2.css";*/
     e.preventDefault();
-    console.log(toDo.value);
+    const value = toDo.value;
+    const id = new Date().getTime().toString();
+    if (value !== "" && editFlag === false) {
+        const element = document.createElement('article');
+        // add class
+        element.classList.add('task-item');
+        // add id
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <input type="checkbox" class="task-checkbox">
+            <button type="button" class="edit-btn">Edit</button>
+            <button type="button" class="delete-btn">Delete</button>
+                <i class="fas fa-trash"></i>
+        </div>`;
+        // append child
+        list.appendChild(element);
+        // show container
+        container.classList.add("show-container");
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+    } else if (value !== "" && editFlag === true) {
+        console.log('editing');
+    } else {
+        console.log('empty value');
+    }
+
+    /*const template = document.querySelector('.task-item.template');
+    const item = template.cloneNode(true);
+    item.classList.remove('template');
+    item.childNodes[1].innerHTML = toDo.value;
+    item.childNodes[3].childNodes[5].addEventListener('click', () => item.remove());
+    container.append(item);*/
 }
 
-// ----- LOCAL STORAGE -----
+// set back to default
+function setBackToDefault() {
+    toDo.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
+}
 
-// ----- SETUP ITEMS -----
+// ***** LOCAL STORAGE *****
+function addToLocalStorage(id, value) {
+    console.log("added to local storage");
+}
+
+// ***** SETUP ITEMS *****
